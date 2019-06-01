7b1e62869eef2a8e33b60faa51eed6cf877bbb45
littleaj <1690572+littleaj@users.noreply.github.com>
2019-06-01 13:13:42 -0700

    create poll id service

---
+++ api/src/main/java/littleaj/simpoll/api/services/PollIdService.java
@@ -0,0 +1,36 @@
+package littleaj.simpoll.api.services;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import littleaj.simpoll.api.repositories.PollRepository;
+import littleaj.simpoll.model.PollId;
+
+@Service
+public class PollIdService {
+    
+    @Autowired
+    private PollRepository pollRepository;
+
+    private final int maxLength;
+
+    public PollIdService(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public PollId generateNextId(String question) {
+        // 1. hyphenate
+        StringBuilder sb = new StringBuilder();
+        String trimmed = StringUtils.strip(question);
+        for (int i = 0; i < question.length(); i++) {
+            // only alphanumeric characters '-' and '_' are allowed, whitespace replaced with hyphen, all others removed
+
+        }
+        // 2. truncate
+        // 3. verify available
+        // 4. numerate
+
+        return null;
+    }
+}
