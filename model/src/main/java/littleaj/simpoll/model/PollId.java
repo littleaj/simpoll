24b4b8ca7f388ce3788c04fb1418c016a221b7c0
littleaj <1690572+littleaj@users.noreply.github.com>
2019-06-01 13:12:23 -0700

    remove behavior from data object

--- model/src/main/java/littleaj/simpoll/model/PollId.java
+++ model/src/main/java/littleaj/simpoll/model/PollId.java
@@ -6,26 +6,9 @@ public class PollId {
 
     private final String id;
 
-    private PollId(String id) {
+    public PollId(String id) {
         this.id = id;
     }
-
-    public static PollId generateNew(String question) {
-        return createIdFromQuestion(question);
-    }
-
-    public static PollId generateNext(PollId id) {
-        return createNextIdFromExisting(id);
-    }
-
-    private static PollId createIdFromQuestion(String question) {
-        throw new UnsupportedOperationException();
-    }
-
-    private static PollId createNextIdFromExisting(PollId id) {
-        throw new UnsupportedOperationException();
-    }
-
 
     @Override
     public boolean equals(Object o) {
