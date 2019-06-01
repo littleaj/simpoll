6f57147278afda45977a99dcbfee74747b848804
littleaj <1690572+littleaj@users.noreply.github.com>
2019-06-01 13:14:46 -0700

    update bean definitions

--- api/src/main/java/littleaj/simpoll/api/SimpollApi.java
+++ api/src/main/java/littleaj/simpoll/api/SimpollApi.java
@@ -9,9 +9,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import littleaj.simpoll.api.repositories.PollRepository;
+import littleaj.simpoll.api.repositories.PollResultsRepository;
+import littleaj.simpoll.api.repositories.PollStatusRepository;
+import littleaj.simpoll.api.repositories.inmem.InMemoryPollRepository;
+import littleaj.simpoll.api.services.PollService;
 
 @SpringBootApplication
 public class SimpollApi {
+
+    private final InMemoryPollRepository repository;
+
+    public SimpollApi() {
+        repository = new InMemoryPollRepository();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SimpollApi.class, args);
     }
@@ -31,6 +42,21 @@ public class SimpollApi {
 
     @Bean
     public PollRepository pollRepository(ApplicationContext ctx) {
-        return new PollRepository();
+        return repository;
+    }
+
+    @Bean
+    public PollResultsRepository pollResultsRepository(ApplicationContext ctx) {
+        return repository;
+    }
+
+    @Bean
+    public PollStatusRepository pollStatusRepository(ApplicationContext ctx) {
+        return repository;
+    }
+
+    @Bean
+    public PollService pollService(ApplicationContext ctx) {
+        return new PollService();
     }
 }
