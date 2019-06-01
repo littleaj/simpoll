a053bb130803eafbaee0df0ee967536c5a205f1d
littleaj <1690572+littleaj@users.noreply.github.com>
2019-06-01 13:07:48 -0700

    add exception for missing poll

---
+++ api/src/main/java/littleaj/simpoll/api/exceptions/PollNotFoundException.java
@@ -0,0 +1,9 @@
+package littleaj.simpoll.api.exceptions;
+
+public class PollNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public PollNotFoundException() {
+        super();
+    }
+}
