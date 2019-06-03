d1f751c01660cff115a9fff496c6bbd6c1684d97
littleaj <1690572+littleaj@users.noreply.github.com>
2019-06-02 17:42:37 -0700

    removed some debug output

--- api/src/main/java/littleaj/simpoll/api/SimpollApi.java
+++ api/src/main/java/littleaj/simpoll/api/SimpollApi.java
@@ -31,15 +31,6 @@ public class SimpollApi {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Inspecting Spring Boot provided beans: ");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-            System.out.println("----------");
             System.out.println("version = "+config.getVersion());
         };
     }
